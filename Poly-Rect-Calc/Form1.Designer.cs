041c4524eb47a094fc3126c81b9aa373dcb77179
LiangRui He <825138000@qq.com>
2012-12-03 12:27:16 +0800

    backup

--- Poly-Rect-Calc/Form1.Designer.cs
+++ Poly-Rect-Calc/Form1.Designer.cs
@@ -43,66 +43,73 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(108, 12);
+            this.button1.Font = new System.Drawing.Font("楷体", 15F);
+            this.button1.Location = new System.Drawing.Point(192, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.Size = new System.Drawing.Size(107, 41);
             this.button1.TabIndex = 0;
-            this.button1.Text = "AddPoint";
+            this.button1.Text = "添加坐标";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(108, 41);
+            this.button2.Font = new System.Drawing.Font("楷体", 15F);
+            this.button2.Location = new System.Drawing.Point(192, 68);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.Size = new System.Drawing.Size(107, 41);
             this.button2.TabIndex = 1;
-            this.button2.Text = "DeletePoint";
+            this.button2.Text = "删除坐标";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(108, 70);
+            this.button3.Font = new System.Drawing.Font("楷体", 15F);
+            this.button3.Location = new System.Drawing.Point(192, 115);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.Size = new System.Drawing.Size(107, 41);
             this.button3.TabIndex = 3;
-            this.button3.Text = "ClearAll";
+            this.button3.Text = "清除";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("宋体", 15F);
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
+            this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(90, 88);
+            this.listBox1.Size = new System.Drawing.Size(121, 124);
             this.listBox1.TabIndex = 4;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 109);
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 151);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.Size = new System.Drawing.Size(29, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "X:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 106);
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox1.Location = new System.Drawing.Point(50, 148);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(69, 21);
+            this.textBox1.Size = new System.Drawing.Size(83, 30);
             this.textBox1.TabIndex = 6;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(33, 133);
+            this.textBox2.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox2.Location = new System.Drawing.Point(50, 194);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(69, 21);
+            this.textBox2.Size = new System.Drawing.Size(83, 30);
             this.textBox2.TabIndex = 8;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
@@ -110,9 +117,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 136);
+            this.label2.Font = new System.Drawing.Font("宋体", 15F);
+            this.label2.Location = new System.Drawing.Point(12, 196);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.Size = new System.Drawing.Size(29, 20);
             this.label2.TabIndex = 7;
             this.label2.Text = "Y:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -120,31 +128,32 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label3.Location = new System.Drawing.Point(13, 203);
+            this.label3.Location = new System.Drawing.Point(13, 230);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.Size = new System.Drawing.Size(0, 15);
             this.label3.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Font = new System.Drawing.Font("楷体", 25F);
+            this.label4.Location = new System.Drawing.Point(10, 254);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.Size = new System.Drawing.Size(100, 34);
             this.label4.TabIndex = 10;
             this.label4.Text = "面积:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(108, 99);
+            this.button4.Font = new System.Drawing.Font("楷体", 15F);
+            this.button4.Location = new System.Drawing.Point(192, 162);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.Size = new System.Drawing.Size(107, 41);
             this.button4.TabIndex = 11;
-            this.button4.Text = "calculate";
+            this.button4.Text = "计算面积";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -152,7 +161,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(211, 226);
+            this.ClientSize = new System.Drawing.Size(330, 297);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -164,9 +173,10 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "凸多边形面积计算器";
             this.ResumeLayout(false);
             this.PerformLayout();
 
